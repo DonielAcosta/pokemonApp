53316d21105210f00bf4c2bd4f76970e481d4147
Doniel Acosta <donielacosta1995@gmail.com>
2025-05-02 10:46:53 -0400

    instalando paquetes react native paper para iconos

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,9 +1,17 @@
 import { Text, View } from 'react-native';
+import { Button } from 'react-native-paper';
 
 export const HomeScreen = () => {
   return (
     <View>
       <Text>HomeScreen</Text>
+      <Button
+      icon="camera"
+      mode="contained"
+      onPress={() => console.log('Pressed')
+      }>
+      Press me
+    </Button>
     </View>
   );
 };
