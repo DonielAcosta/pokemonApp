8f170ae2dc5cb83fc415e55ed0b854773f79713e
Doniel Acosta <donielacosta1995@gmail.com>
2025-05-02 18:12:31 -0400

    peticion de api guardada en cache con interface de respuesta

--- src/actions/pokemons/get-pokemons.ts
+++ src/actions/pokemons/get-pokemons.ts
@@ -1,19 +1,27 @@
 import { pokeApi } from '../../config/api/pokeApi';
-import { Pokemon } from '../../domain/entities/pokemon';
+import type { Pokemon } from '../../domain/entities/pokemon';
+import  type { PokeAPIPaginatedResponse, PokeAPIPokemon } from '../../infrastructure/interfaces/pokepi.interfaces';
 
 
 
 export const sleep = async() =>{
     return new Promise(resolve => setTimeout(resolve,200));
 };
-export const getPokemons = async ():Promise<Pokemon[]> =>{
+export const getPokemons = async (page: number, limit: number = 20):Promise<Pokemon[]> =>{
     await sleep();
     try {
-        const url = '/pokemon';
-        const { data } = await pokeApi.get(url);
-        console.log(data);
+        const url = `/pokemon?offset=${ page * 10 }&limit=${limit}`;
+        const { data } = await pokeApi.get<PokeAPIPaginatedResponse>(url);
+
+        const pokemonPromises = data.results.map( (info) =>{
+            return pokeApi.get<PokeAPIPokemon>(info.url);
+        });
+        const pokeApiPokemons = await Promise.all(pokemonPromises);
+        // console.log(pokeApiPokemons);
+        // console.log(data);
         return[];
     } catch (error) {
+        console.log(error);
         throw new Error('Error Getting Pokemons');
     }
 };
