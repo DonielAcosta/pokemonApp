cfe29d119c5c29ad7f04dffa531c082cca8c437b
Doniel Acosta <donielacosta1995@gmail.com>
2025-05-02 20:25:15 -0400

    cargando la data de la peticion

--- src/actions/pokemons/get-pokemons.ts
+++ src/actions/pokemons/get-pokemons.ts
@@ -1,6 +1,7 @@
 import { pokeApi } from '../../config/api/pokeApi';
 import type { Pokemon } from '../../domain/entities/pokemon';
 import  type { PokeAPIPaginatedResponse, PokeAPIPokemon } from '../../infrastructure/interfaces/pokepi.interfaces';
+import { PokemonMapper } from '../../infrastructure/mappers/pokemon.mapper';
 
 
 
@@ -10,16 +11,36 @@ export const sleep = async() =>{
 export const getPokemons = async (page: number, limit: number = 20):Promise<Pokemon[]> =>{
     await sleep();
     try {
-        const url = `/pokemon?offset=${ page * 10 }&limit=${limit}`;
+        // const url = `/pokemon?offset=${ page * 10 }&limit=${limit}`;
+        // const { data } = await pokeApi.get<PokeAPIPaginatedResponse>(url);
+
+        // const pokemonPromises = data.results.map( (info) =>{
+        //     return pokeApi.get<PokeAPIPokemon>(info.url);
+        // });
+        // const pokeApiPokemons = await Promise.all(pokemonPromises);
+        // const pokemons = pokeApiPokemons.map((item) => PokemonMapper.pokeApiPokemonToEntity(item.data));
+        // // console.log(pokeApiPokemons);
+        // // console.log(data);
+        // console.log(pokemons[0]);
+        // return pokemons;
+
+        const url = `/pokemon?offset=${page * 10}&limit=${limit}`;
         const { data } = await pokeApi.get<PokeAPIPaginatedResponse>(url);
 
-        const pokemonPromises = data.results.map( (info) =>{
-            return pokeApi.get<PokeAPIPokemon>(info.url);
-        });
+        const pokemonPromises = data.results.map(info =>
+          pokeApi.get<PokeAPIPokemon>(info.url)
+        );
+
         const pokeApiPokemons = await Promise.all(pokemonPromises);
-        // console.log(pokeApiPokemons);
-        // console.log(data);
-        return[];
+
+        const pokemons = await Promise.all(
+          pokeApiPokemons.map(item =>
+            PokemonMapper.pokeApiPokemonToEntity(item.data)
+          )
+        );
+        // const pokemons = pokeApiPokemons.map((item) => PokemonMapper.pokeApiPokemonToEntity(item.data));//marca error al retornar entonces coloque la de arriba
+        console.log(pokemons[0]);
+        return pokemons;
     } catch (error) {
         console.log(error);
         throw new Error('Error Getting Pokemons');
